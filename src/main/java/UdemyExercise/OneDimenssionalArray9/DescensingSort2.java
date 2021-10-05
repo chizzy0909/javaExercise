6fec4b625da057c03ad7ea7b43e0295416ee088f
Cheng Hongzhen <chizzy0907@gmail.com>
2021-10-05 16:26:55 +0900

    完成作业9的2

---
+++ src/main/java/UdemyExercise/OneDimenssionalArray9/DescensingSort2.java
@@ -0,0 +1,28 @@
+package UdemyExercise.OneDimenssionalArray9;
+
+//编写程序，读取10 个整数，然后按照和读入顺序相反的顺序将它们显示出来。
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DescensingSort2 {
+
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter 10 integers: ");
+    int[] number = new int[10];
+
+    for (int i = 0; i < number.length; i++) {
+      number[i] = input.nextInt();
+    }
+    reverse(number);
+  }
+
+  public static void reverse(int[] array) {
+    for (int i = array.length - 1; i >= 0; i--) {
+      System.out.print(array[i] + " ");
+    }
+  }
+
+}
